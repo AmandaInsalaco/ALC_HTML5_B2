41c468ddf540b25c30172c0d2332291e14209d57
Unknown <mandee.insalaco@gmail.com>
2019-01-22 10:29:42 -0700

    Added var pcName

---
+++ Adventure_Game/game.js
@@ -0,0 +1,16 @@
+//document.write("Hello World!");
+//document.write("<h1>Everything is AWESOME!</h1>");
+//single line comment
+/*
+    alert box, will pop up with okay button
+    alert("Warning! Will Robinson...Warning!");
+    confirmation box will be okay button or cancel button
+    confirm("Do you like pokemon?");
+    prompt takes writen feedback
+    prompt("What is your favorite pokemon?");
+*/
+
+
+var pcName = prompt("What is your name, zombie killer?");
+
+document.write(pcName);
